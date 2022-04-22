3d15d152789e75148c6d8f9d8df11333c71aaba9
Denis <lesobrod@yandex.ru>
2022-04-22 07:34:29 +0300

    1.4

--- client.py
+++ client.py
@@ -1,13 +1,20 @@
 import asyncio
 import websockets
+from websockets import WebSocketServerProtocol
 
 
-async def test():
-    async with websockets.connect("ws://localhost:8000") as websocket:
-        await websocket.send("hello")
-
-        response = await websocket.recv()
-        print(response)
+async def consumer_handler(ws: WebSocketServerProtocol) -> None:
+    async for message in ws:
+        print(message)
 
 
-asyncio.get_event_loop().run_until_complete(test())
+async def consume(host: str, port: int) -> None:
+    url = f'ws://{host}:{port}'
+    async with websockets.connect(url) as ws:
+        await consumer_handler(ws)
+
+
+if __name__ == '__main__':
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(consume(host='localhost', port=8000))
+    loop.run_forever()
--- server.py
+++ server.py
@@ -1,15 +1,37 @@
 import asyncio
 import websockets
+from websockets import WebSocketServerProtocol
 
 
-# create handler for each connection
+class Server:
+    clients = set()
 
-async def handler(websocket, path):
-    data = await websocket.recv()
-    reply = f"Data recieved as:  {data}!"
-    await websocket.send(reply)
+    async def register(self, ws: WebSocketServerProtocol) -> None:
+        self.clients.add(ws)
+        print(f'{ws.remote_address} connect')
+
+    async def unregister(self, ws: WebSocketServerProtocol) -> None:
+        self.clients.remove(ws)
+        print(f'{ws.remote_address} disconnect')
+
+    async def distribute(self, ws: WebSocketServerProtocol) -> None:
+        async for message in ws:
+            await self.send_to_clients(message)
+
+    async def send_to_clients(self, message: str) -> None:
+        if self.clients:
+            await asyncio.wait([client.send(message) for client in self.clients])
+
+    async def ws_handler(self, ws: WebSocketServerProtocol, url: str) -> None:
+        await self.register(ws)
+        try:
+            await  self.distribute(ws)
+        finally:
+            await  self.unregister(ws)
 
 
-start_server = websockets.serve(handler, "localhost", 8000)
-asyncio.get_event_loop().run_until_complete(start_server)
-asyncio.get_event_loop().run_forever()
+server = Server()
+start_server = websockets.serve(server.ws_handler, 'localhost', 8000)
+loop = asyncio.get_event_loop()
+loop.run_until_complete(start_server)
+loop.run_forever()
